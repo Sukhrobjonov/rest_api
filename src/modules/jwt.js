afca44b78b8292e2f11935d2c94d49e8bc526c90
Sukhrobjonov <javohir200903@gmail.com>
2021-10-09 20:59:19 +0500

    add jwt

---
+++ src/modules/jwt.js
@@ -0,0 +1,15 @@
+import { sign, verify } from "jsonwebtoken";
+
+export default function createToken(token) {
+    return verify(token, process.env.SECRET_KEY);
+}
+
+
+export default function checkToken(token) {
+    try {
+        return verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
